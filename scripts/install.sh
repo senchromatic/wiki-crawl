b17afab02c37a1b7cc5e7dca7e3fd0715bcbe8ec
senchromatic <samuelfzhang@gmail.com>
2017-06-21 23:10:17 -0400

    remove irrelevant suggestion

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ sudo pip install graphviz
 sudo pip install pydot
 
 # R
-echo "Please manually install the version of R and RStudio appropriate for your system requirements (Penn's workstations should have both already installed)"
+echo "Please manually install the version of R and RStudio appropriate for your system requirements"
 
 # C++14
 sudo apt-get install build-essential
